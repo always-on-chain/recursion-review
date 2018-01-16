1303f2e6ed82001bbe5188ebc4cc8ce2cad43cec
waynekwest88 <waynekwest88@gmail.com>
2018-01-15 20:23:42 -0800

    Implemented recursive getElements by classname using children to only act on the elements instead of the full nodes returned by childNodes

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -2,9 +2,51 @@
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
 // };
+/* 
+You should use document.body, element.childNodes, and element.classList
+
+*/
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, node) {
+  var node =  node || document.body;
+  var results = [];
+  
+  // basecase
+  if (node.classList && node.classList.contains(className)) {
+    results.push(node);
+  }
+ 
+  for (var i = 0; i < node.children.length; i++) {
+    var childNodes = getElementsByClassName(className, node.children[i]);
+    results = results.concat(childNodes);
+  }
+  
+  
+  return results;
 };
+
+/*
+1. START with body
+we need to check if it contains the classname
+we also need to check if the nodes have the classname
+and check if the nodes nodes have a class name
+so if we loop over the node and recursively check with our if statement to push it
+  1a. push with result if meets .classList and .classlist.contains(className)
+  2a.
+
+
+
+
+*/
+
+    
+    // else if (node.childNodes[i].classList) {
+    //    getElementsByClassName(className, node.childNodes[i], results);
+    // }
+    // else {
+    //   getElementsByClassName(className, node.childNodes[i], results);
+    // }
+
+
+
